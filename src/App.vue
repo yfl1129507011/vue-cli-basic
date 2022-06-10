ce7db1e7373adb17440760c256bc4cd436472c32
fenlon <1129507011@qq.com>
2022-06-10 15:40:56 +0800

    Element-UI组件库入门

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,36 @@
 <template>
     <div>
-        <div class="row">
-            <Banner/>
+        <button>原生的button</button>
+        <input type="text">
+        <el-row>
+            <el-button>默认按钮</el-button>
+            <el-button type="primary">主要按钮</el-button>
+            <el-button type="success">成功按钮</el-button>
+            <el-button type="info">信息按钮</el-button>
+            <el-button type="warning">警告按钮</el-button>
+            <el-button type="danger">危险按钮</el-button>
+        </el-row>
+        <div class="block">
+            <span class="demonstration">默认</span>
+            <el-date-picker
+                type="date"
+                placeholder="选择日期">
+            </el-date-picker>
         </div>
-        <div class="row">
-            <div class="col-xs-2 col-xs-offset-2">
-                <div class="list-group">
-                    <!--原始使用a标签跳转多个页面,多页面应用-->
-                    <!-- <a class="list-group-item active" href="./about.html">About</a>
-                    <a class="list-group-item" href="./home.html">Home</a> -->
-
-                    <!--vue中借助router=link标签实现路由的切换-->
-                    <router-link replace class="list-group-item" active-class="active" to="/about">About</router-link>
-                    <router-link replace class="list-group-item" active-class="active" to="/home">Home</router-link>
-                </div>
-            </div>
-            <div class="col-xs-6">
-                <div class="panel">
-                    <div class="panel-body">
-                        <!-- 指定组件呈现的位置 -->
-                        <router-view></router-view>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <el-row>
+            <el-button icon="el-icon-search" circle></el-button>
+            <el-button type="primary" icon="el-icon-edit" circle></el-button>
+            <el-button type="success" icon="el-icon-check" circle></el-button>
+            <el-button type="info" icon="el-icon-message" circle></el-button>
+            <el-button type="warning" icon="el-icon-star-off" circle></el-button>
+            <el-button type="danger" icon="el-icon-delete" circle></el-button>
+        </el-row>
     </div>
 </template>
 
 <script>
-import Banner from './components/Banner'
 
 export default {
     name: 'App',
-    components: {
-        Banner
-    }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -6,20 +6,30 @@ import Vue from 'vue'
 // 引入App组件，它是所有组件的父组件
 import App from './App.vue'
 
-// 引入路由插件
-import VueRouter from 'vue-router'
-import router from './router'
+// 引入ElementUI组件库
+// import ElementUI from 'element-ui'
+// 引入ElementUI全部样式
+// import 'element-ui/lib/theme-chalk/index.css'
+
+// Element按需引入
+import {Button, Row, DatePicker} from 'element-ui'
 
 // 关闭vue的生产提示
 Vue.config.productionTip = false
 
-Vue.use(VueRouter)
+// 应用ElementUI
+// Vue.use(ElementUI)
+Vue.component(Button.name, Button)
+Vue.component('el-row', Row)
+Vue.component(DatePicker.name, DatePicker)
+/* 或写为
+ * Vue.use(Button)
+ */
 
 new Vue({
   // 将App组件放入容器中
   render: h => h(App),
   beforeCreate(){
     Vue.prototype.$bus = this   // 安装全局事件总线
-  },
-  router: router
+  }
 }).$mount('#app')
